2dccc36f22dc924d402b8d4302f359344142264b
archieyao <archieyao@tencent.com>
2022-06-15 22:09:17 +0800

    [feat] add merge sort

--- src/sort/merge_sort.go
+++ src/sort/merge_sort.go
@@ -1,6 +1,7 @@
 package sort
 
-func mergSort(arr []int) {
+// 算法流程：先保证左边部分有序，然后保证右边部分有序，最后合并左右两部分
+func mergeSort(arr []int) {
 	process(arr, 0, len(arr)-1)
 }
 
@@ -14,6 +15,9 @@ func process(arr []int, l int, r int) {
 	merge(arr, l, mid, r)
 }
 
+// 合并过程中，分别用两个指针遍历左右集合，然后开辟一个新数组，分别比较左右集合的元素大小，直到其中一个集合遍历结束
+// 一方集合遍历结束之后，需要继续将另一集合数据加入到新数组中
+// 最后将新数组中的元素放到最终的数组中去，
 func merge(arr []int, l int, mid int, r int) {
 	newArr := make([]int, r-l+1)
 	p1 := l
@@ -43,4 +47,34 @@ func merge(arr []int, l int, mid int, r int) {
 	for i := 0; i < len(newArr); i++ {
 		arr[l+i] = newArr[i]
 	}
+}
+
+func mergeSort1(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	mid := len(arr) / 2
+	return merge1(mergeSort1(arr[0:mid]), mergeSort1(arr[mid:]))
+}
+
+func merge1(arrL []int, arrR []int) []int {
+	var result []int
+	for len(arrL) > 0 && len(arrR) > 0 {
+		if arrL[0] < arrR[0] {
+			result = append(result, arrL[0])
+			arrL = arrL[1:]
+		} else {
+			result = append(result, arrR[0])
+			arrR = arrR[1:]
+		}
+	}
+	for len(arrL) > 0 {
+		result = append(result, arrL[0])
+		arrL = arrL[1:]
+	}
+	for len(arrR) > 0 {
+		result = append(result, arrR[0])
+		arrR = arrR[1:]
+	}
+	return result
 }
--- src/sort/merge_sort_test.go
+++ src/sort/merge_sort_test.go
@@ -1,9 +1,17 @@
 package sort
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_mergSort(t *testing.T) {
 	arr := []int{2, 4, 5, 45, 7, 56, 2, 24, 354, 345, 21, 1, 6, -2}
-	mergSort(arr)
+	mergeSort(arr)
 	t.Logf("%v", arr)
+}
+
+func Test_mergeSort1(t *testing.T) {
+	arr := []int{2, 4, 5, 45, 7, 56, 2, 24, 354, 345, 21, 1, 6, -2}
+	i := mergeSort1(arr)
+	t.Logf("%v", i)
 }
